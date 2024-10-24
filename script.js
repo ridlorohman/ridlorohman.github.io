c7c99305227a94ad2b5e645c3d5045bf4462cad9
ridlorohman <73713579+ridlorohman@users.noreply.github.com>
2024-10-24 21:34:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+
+const slideImages = document.querySelectorAll('.slide-image');
+const slideIndicators = document.querySelectorAll('.slide-indicator');
+
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+  slideImages.forEach((image, index) => {
+    if (index === slideIndex) {
+      image.style.display = 'block';
+    } else {
+      image.style.display = 'none';
+    }
+  });
+
+  slideIndicators.forEach((indicator, index) => {
+    if (index === slideIndex) {
+      indicator.classList.add('active');
+    } else {
+      indicator.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide++;
+  if (currentSlide >= slideImages.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
+
+var toggleButton = document.getElementById('toggleButton');
+var floatingScreen = document.getElementById('floatingScreen');
+
+toggleButton.addEventListener('click', function() {
+    if (floatingScreen.style.display === 'none') {
+        floatingScreen.style.display = 'block';
+    } else {
+        floatingScreen.style.display = 'none'
+    }
+});
